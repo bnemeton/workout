ccf31ca360f56cc5e100f657375e26967c5b9426
Joe <jmwinitsky@gmail.com>
2021-05-24 15:46:43 -0700

    added routes

---
+++ controllers/workoutRoutes.js
@@ -0,0 +1,75 @@
+const Workout = require('../models/workout.js')
+const router = require('express').Router();
+
+router.post('/api/workouts', (req, res) => {
+    Workout.create({})
+        .then ((workout) => {
+            res.json(workout)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+router.get('/api/workouts', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            } 
+        }
+    ])
+    .then(workouts => {
+        res.json(workouts)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.put('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        {$push: {exercises: req.body}},
+        {new: true, runValidators: true}
+    )
+    .then(workout => {
+        res.json(workout)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.delete('/api/workouts', (req, res) => {
+    Workout.findByIdAndDelete(req.body.id)
+    .then(() => {
+        res.json(true)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ])
+    .sort({_id: -1})
+    .limit(7)
+    .then(workouts => {
+        res.json(workouts)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+module.exports = router;
